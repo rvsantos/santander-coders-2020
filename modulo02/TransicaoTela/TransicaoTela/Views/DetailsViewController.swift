1a47a98c0450e1125c2951ce59aab28ed4e1ceb2
Rafael V. dos Santos <rvsantos00@gmail.com.br>
2020-09-28 19:22:04 -0300

    Implementei a mudança de tela via code.

---
+++ modulo02/TransicaoTela/TransicaoTela/Views/DetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.swift
+//  TransicaoTela
+//
+//  Created by Rafael V. dos Santos on 25/09/20.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    //MARK: - @IBOutlet
+    @IBOutlet weak var myLabel: UILabel!
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }    
+}
--- modulo02/TransicaoTela/TransicaoTela/Views/FirstViewController.swift
+++ modulo02/TransicaoTela/TransicaoTela/Views/FirstViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,5 +30,31 @@ class FirstViewController: UIViewController {
     
     override func viewDidDisappear(_ animated: Bool) {
         print("ViewDidDisappear")
+    }
+    
+    //MARK: - Functions
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let detailsViewController: DetailsViewController? = segue.destination as? DetailsViewController
+        
+        guard let vc: DetailsViewController = segue.destination as? DetailsViewController else {return}
+        vc.view.backgroundColor = .brown
+        vc.myLabel.text = "Rafael"
+        
+//        if let vc = detailsViewController {
+//            vc.view.backgroundColor = .red
+//        }
+    }
+    
+    //MARK: - @IBAction
+    @IBAction func tappedLoginButton(_ send: UIButton) {
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//
+//        let vc: DetailsViewController? = storyboard.instantiateViewController(identifier: "DetailsViewController") as? DetailsViewController
+//        vc?.view.backgroundColor = .purple
+//        vc?.myLabel.text = "Rafael"
+                
+//        self.present(vc ?? UIViewController(), animated: true, completion: nil)
+        
+        self.performSegue(withIdentifier: "DetailsViewController", sender: nil)
     }
 }
